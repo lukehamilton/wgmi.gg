e6a2b02033c09005714ca4fea12f2f466c012928
Luke Hamilton <lukehamiltonmail@gmail.com>
2021-11-11 22:17:12 -0500

    fix input color

--- components/list.js
+++ components/list.js
@@ -36,7 +36,7 @@ const List = () => {
               <input
                 id="search"
                 name="search"
-                class="block w-full bg-white border border-gray-300 rounded-md py-2 pl-10 pr-3 text-sm placeholder-gray-500 focus:outline-none focus:text-gray-900 focus:placeholder-gray-400 focus:ring-1 focus:ring-rose-500 focus:border-rose-500 sm:text-sm"
+                class="block w-full bg-white border text-gray-900 border-gray-300 rounded-md py-2 pl-10 pr-3 text-sm placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-rose-500 focus:border-rose-500 sm:text-sm"
                 placeholder="Search"
                 type="search"
                 onChange={(e) => setQuery(e.target.value)}
